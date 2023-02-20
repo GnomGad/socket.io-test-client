d516d8bf69dcbd2ede8199db454d4e3646de7ea4
Zhenya <Canimore@yandex.com>
2023-02-20 22:29:33 +0300

    chat bot client update

--- assets/main.js
+++ assets/main.js
@@ -6,18 +6,36 @@ const messages = document.querySelector(".messages");
 const form = document.querySelector(".form");
 const input = document.querySelector(".input");
 const nameBlock = document.querySelector(".name");
-const userName = prompt("ваше имя:");
+const userName = "123"; // prompt("ваше имя:");
 nameBlock.innerHTML = `${userName}`;
 
+function writeMessage(data) {
+    const item = document.createElement("li");
+    const date = new Date();
+    item.innerHTML = `<span>[${date.toLocaleTimeString("ru-RU", {
+        timeZone: "Europe/Moscow",
+    })}]</span> <span><b>${data.name}</b></span>: ${data.message}`;
+    messages.appendChild(item);
+}
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     if (input.value) {
-        socket.emit("chat message", { message: input.value, name: userName });
+        socket.emit("client-to-server-message", {
+            id: socket.id,
+            message: input.value,
+            name: userName,
+        });
+        writeMessage({ name: userName, message: input.value });
         input.value = "";
     }
 });
-socket.on("chat message", (data) => {
-    const item = document.createElement("li");
-    item.innerHTML = `<span>${data.name}</span>:  ${data.message}`;
-    messages.appendChild(item);
+socket.on("connect", () => {
+    console.log(socket.id);
+});
+socket.on("disconnect", () => {
+    console.log("disconnect"); // undefined
+});
+socket.emit("hello", "world");
+socket.on("server-to-client-message", (data) => {
+    writeMessage(data);
 });
